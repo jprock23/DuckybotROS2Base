77afb715857526c179996f31c914e452120f6660
jprock23 <juanproque@outlook.com>
2025-10-20 21:45:23 -0500

    formatting tweaks

--- src/controller/controller/controller_node.py
+++ src/controller/controller/controller_node.py
@@ -12,61 +12,60 @@ class Controller_Node(Node):
     def __init__(self):
         super().__init__('controller_node')
         
-        kP_linear_l, kI_linear_l, kD_linear_l = 6.0, 0.0, 0.0
-        kP_linear_r, kI_linear_r, kD_linear_r = 7.5, 0.0, 0.0
-        #l: 13.0 r: 14.8
+        # --- Parameters ---
+        self.declare_parameter('kp', 2.1)
+        self.declare_parameter('ki', 0.7)
+        self.declare_parameter('kd', 0.0)
+        self.declare_parameter('output_limits', [-1.0, 1.0])  # throttle range
+        control_period = 1/10
 
-        self.left_linear_controller = PID(kP_linear_l, kI_linear_l, kD_linear_l)
-        self.right_linear_controller = PID(kP_linear_r, kI_linear_r, kD_linear_r)
-        
-        # kP_angular, kI_angular, kD_angular = 13.0, 0.0, 0.0
-        # self.angular_controller = PID(kP_angular, kI_angular, kD_angular)
-        
-        #Subscribers
-        self.cmd_subscription = self.create_subscription(WheelsCmdStamped, '/wheels_cmd', self.set_linear_setpoint, 10)
+        kp = self.get_parameter('kp').get_parameter_value().double_value
+        ki = self.get_parameter('ki').get_parameter_value().double_value
+        kd = self.get_parameter('kd').get_parameter_value().double_value
+        output_limits = self.get_parameter('output_limits').get_parameter_value().double_array_value
+        output_limits = tuple(output_limits) if output_limits else (-1.0, 1.0)
+
+        # Controllers
+        self.pid_left = PID(kp + 0.05, ki, kd, setpoint=0, sample_time=control_period)
+        self.pid_right = PID(kp, ki, kd, setpoint=0, sample_time=control_period)
+        self.pid_left.output_limits = output_limits
+        self.pid_right.output_limits = output_limits
+
+        # Subscribers
+        self.create_subscription(TwistStamped, '/left_encoder_node/velocity', self.update_left_vel, 10)
+        self.create_subscription(TwistStamped, '/right_encoder_node/velocity', self.update_right_vel, 10)
+        self.create_subscription(WheelsCmdStamped, '/wheels_cmd', self.set_linear_setpoint, 10)
         # self.angular_cmd_subscription = self.create_subscription(TwistStamped, '/angular_cmd', self.set_angular_setpoint, 10)
-        self.velocity_left_subscription = self.create_subscription(TwistStamped, '/left_encoder_node/velocity', self.update_left_vel, 10)
-        self.velocity_right_subscription = self.create_subscription(TwistStamped, '/right_encoder_node/velocity', self.update_right_vel, 10)
 
-        #Publishers
+        # Publishers
+        self.throttle_pub = self.create_publisher(Throttle, '/throttles', 10)
         self.executed_cmd_publisher = self.create_publisher(WheelsCmdStamped, '/wheels_cmd_executed', 10)
-        self.throttle_publisher = self.create_publisher(Throttle, '/throttles', 10)
         self.twist_publisher = self.create_publisher(TwistStamped, '/robot_twist', 10)
+
+        # Internal state
+        self.current_left_vel = 0.0
+        self.current_right_vel = 0.0
+        self.target_left_vel = 0.0
+        self.target_right_vel = 0.0
+        self.last_left_time = None
+        self.last_right_time = None
 
         self.wheel_radius = .0325
         self.track_width = 0.14
-
-        self.prev_Lticks = None
-        self.prev_Rticks = None
-
-        self.curr_velL = 0.0
-        self.curr_velR = 0.0
-        self.angular_vel = 0.0
-        self.linear_vel = 0.0
-        self.target_omega = 0.0
-        
         self.max_linear_vel = 0.40
         self.max_angular_vel = 0.25
 
-        self.left_throttle = 0.0
-        self.right_throttle = 0.0
-
-        self.timer_period = 1/30.0
-        # self.twist_timer = self.create_timer(self.timer_period, self.calculate_twist)
-        self.linear_control_timer = self.create_timer(self.timer_period, self.calculate_linear_control)
-        # self.angular_control_timer = self.create_timer(self.timer_period, self.calculate_angular_control)
-
-        self.left_linear_controller.sample_time = self.timer_period
-        self.right_linear_controller.sample_time = self.timer_period
+        #timer
+        self.timer = self.create_timer(control_period, self.calculate_linear_control)
 
 
     def update_left_vel(self, msg: TwistStamped):
-        self.curr_velL = msg.twist.linear.x
-
+        self.current_left_vel = msg.twist.linear.x
+        self.last_left_time = msg.header.stamp
 
     def update_right_vel(self, msg: TwistStamped):
-        self.curr_velR = msg.twist.linear.x
-
+        self.current_right_vel = msg.twist.linear.x
+        self.last_right_time = msg.header.stamp
 
     def calculate_twist(self):
         twist_msg = TwistStamped()
@@ -96,33 +95,33 @@ class Controller_Node(Node):
         
 
     def calculate_linear_control(self):
-        self.left_throttle = self.left_linear_controller(self.curr_velL)
-        self.right_throttle = self.right_linear_controller(self.curr_velR)
-        # self.left_throttle = 1.0
-        # self.right_throttle = 1.0
-        throttle_msg = Throttle()
-        throttle_msg.left_throttle = max(-1.0, min(self.left_throttle, 1.0))
-        throttle_msg.right_throttle = max(-1.0, min(self.right_throttle, 1.0))
-        self.throttle_publisher.publish(throttle_msg)
+        left_throttle = self.pid_left(self.current_left_vel)
+        right_throttle = self.pid_right(self.current_right_vel)
 
+        if self.pid_left.setpoint == 0.0:
+            left_throttle = 0.0
+        if self.pid_right.setpoint == 0.0:
+            right_throttle = 0.0
+
+        throttle_msg = Throttle()
+        throttle_msg.left_throttle = float(left_throttle)
+        throttle_msg.right_throttle = float(right_throttle)
 
         executed_msg = WheelsCmdStamped()
         executed_msg.header.stamp = self.get_clock().now().to_msg()
         executed_msg.header.frame_id = 'base_link'
-        executed_msg.vel_left = self.left_throttle
-        executed_msg.vel_right = self.right_throttle
+        executed_msg.vel_left = left_throttle
+        executed_msg.vel_right = right_throttle
         self.executed_cmd_publisher.publish(executed_msg)
+
+        self.throttle_pub.publish(throttle_msg)
 
         
     def set_linear_setpoint(self, msg: WheelsCmdStamped):
-        """callback that subscirbes to the /wheels_cmd topic to get and apply motor controls"""
-        self.get_logger().info(f'Time stamp: {msg.header.stamp.sec}, Frame_id: {msg.header.frame_id}, vel_left: {msg.vel_left}, vel_right: {msg.vel_right}')
-
-        self.left_linear_controller.setpoint = max(-self.max_linear_vel, min(msg.vel_left, self.max_linear_vel))
-        self.right_linear_controller.setpoint = max(-self.max_linear_vel, min(msg.vel_right, self.max_linear_vel))
-
-        print("left_set::", self.left_linear_controller.setpoint)
-        print("right_set::", self.right_linear_controller.setpoint)
+        self.target_left_vel = msg.vel_left
+        self.target_right_vel = msg.vel_right
+        self.pid_left.setpoint = self.target_left_vel
+        self.pid_right.setpoint = self.target_right_vel
 
 
     def set_angular_setpoint(self, msg: TwistStamped):
@@ -133,12 +132,13 @@ class Controller_Node(Node):
 
 def main():
     rclpy.init()
-    
     controller_node = Controller_Node()
-    
-    rclpy.spin(controller_node)
-    
-    rclpy.shutdown()
-    
+    try:
+        rclpy.spin(controller_node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        controller_node.destroy_node()
+        rclpy.shutdown()
 if __name__ == "__main__":
     main()
